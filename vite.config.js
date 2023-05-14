a9417549ced8b46cd7ab1190bf968fc634995f35
bc464 <bclaassen9@gmail.com>
2023-05-14 12:31:20 +0200

    deleted config deploy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
-    base: '/',
+    plugins: [react()], 
   }
-
-  if (command !== 'serve') {
-    config.base = '/react-minimalist-project/'
-  }
-
-  return config
 })
 
