91517a9b9b9d2fdc14f395a7803ef491b8f421cb
bc464 <bclaassen9@gmail.com>
2023-05-14 12:14:01 +0200

    changed App.js

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/react-minimalist-profile/'}>
     <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
